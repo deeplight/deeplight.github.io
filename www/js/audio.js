e65caba29a226df72c3bc1d82e7df1534c1a3e37
qujianbing@gmail.com <qujianbing@gmail.com>
2017-04-28 11:59:28 -0500

    al

--- www/js/audio.js
+++ www/js/audio.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var audioContext = new AudioContext();
+var audioCtx = new AudioContext();
 
 function hasGetUserMedia() {
     return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
@@ -8,7 +8,22 @@ function hasGetUserMedia() {
 }
 
 if (hasGetUserMedia()) {
-    // Good to go!
+    alert('getUserMedia ok');
 } else {
     alert('getUserMedia() is not supported in your browser');
+}
+
+
+function playByte(b) {
+    console.log('PlayByte-' + b);
+
+    var oscillator = audioCtx.createOscillator();
+
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 440 * b; // value in hertz
+    oscillator.connect(audioCtx.destination);
+    oscillator.start();
+    setTimeout(function() {
+        oscillator.stop(0);
+    }, 1000);
 }

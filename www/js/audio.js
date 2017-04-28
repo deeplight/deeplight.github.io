641059faf5a224d3c313875c84b6720836536111
qujianbing@gmail.com <qujianbing@gmail.com>
2017-04-28 11:28:17 -0500

    add audio

---
+++ www/js/audio.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var audioContext = new AudioContext();
+
+function hasGetUserMedia() {
+    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+if (hasGetUserMedia()) {
+    // Good to go!
+} else {
+    alert('getUserMedia() is not supported in your browser');
+}
